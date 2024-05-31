af8dee435a82092a648a80e58fa7c374c88bd649
LabatiJulien <julien.labati@hotmail.fr>
2024-05-31 19:44:12 +0200

    Téléchargement Cv

--- portfolio/src/components/Profil.js
+++ portfolio/src/components/Profil.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import smileyImage from '../img/Smiley transmetropolitan.png';
 import backgroundImage from '../img/voluteFuméeViolet.jpeg';
+import cvPDF from '../Cv/CV_Labati_Julien.pdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons'; 
 
 const Profil = () => {
     return (
         <div id="profil" className="profil" style={{ backgroundImage: `url(${backgroundImage})` }}>
-             <div className="background-image"></div>
-            <div className="content"></div>
-            <h2>
-                Bienvenue sur mon Profil
+            <div className="content">
+                <h2>Bienvenue sur mon Profil</h2>
                 <img src={smileyImage} alt="Smiley" className="bounce-icon" />
-            </h2>
-            <p>Labati julien  </p>
-            <p>Intégrateur Web</p>
-            <p>Voici mon Cv : </p>
-            <p>Compétences : [Vos compétences]</p>
+                <p>Labati julien  </p>
+                <p>Intégrateur Web</p>
+                <p>Voici mon CV : <a href={cvPDF} download><button className="button-download"><FontAwesomeIcon icon={faDownload} /></button></a></p>
+                <p>Compétences : [Vos compétences]</p>
+            </div>
         </div>
     );
 }
