24468458f9a3dd7ca7b1c2476c2fffe286154817
LabatiJulien <julien.labati@hotmail.fr>
2024-05-31 09:48:24 +0200

    Creation composant de base

---
+++ portfolio/src/components/Profil.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profil = () => {
+    return (
+        <div className="profil">
+            <h2>Profil de Développeur</h2>
+            <p>Formation : [Votre formation]</p>
+            <p>Compétences : [Vos compétences]</p>
+        </div>
+    );
+}
+
+export default Profil;
---
+++ portfolio/src/components/Projets.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Projets = () => {
+    return (
+        <div className="projets">
+            <h2>Projets</h2>
+            <div className="projet">
+                <h3>Nom du Projet 1</h3>
+                <p>Description : [Description du Projet]</p>
+                <p>Compétences développées : [Compétences]</p>
+                <a href="[Lien vers le code ou le site]" target="_blank" rel="noopener noreferrer">Voir le Projet</a>
+            </div>
+            <div className="projet">
+                <h3>Nom du Projet 2</h3>
+                <p>Description : [Description du Projet]</p>
+                <p>Compétences développées : [Compétences]</p>
+                <a href="[Lien vers le code ou le site]" target="_blank" rel="noopener noreferrer">Voir le Projet</a>
+            </div>
+        </div>
+    );
+}
+
+export default Projets;
