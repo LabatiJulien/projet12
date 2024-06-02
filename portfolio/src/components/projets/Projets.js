a9dd1e44b081660ad91e1d8b5b240d5b5c6ac747
LabatiJulien <julien.labati@hotmail.fr>
2024-06-02 10:58:08 +0200

    Création carroussel projet, ajout Slick

--- portfolio/src/components/projets/Projets.js
+++ portfolio/src/components/projets/Projets.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import './Projets.css'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Projets.css';
+import ohMyFoodImage from '../../img/oh-my-food.png';
 
 const Projets = () => {
+    const projet = {
+        nom: "Ohmyfood",
+        description: "Projet 4 OpenClassroom: Ohmyfood. J'ai conçu un menu alimentaire totalement adaptable à tous les appareils, en priorisant d'abord le mobile. J'ai développé un système de chargement ainsi que quelques animations CSS pour permettre la sélection d'un menu et l'utilisation du bouton 'j'aime'.",
+        competences: [
+            "Capacité à créer des interfaces web mobile-first",
+            "Maîtrise de Sass pour structurer et optimiser le CSS",
+            "Capacité à utiliser Git et GitHub pour la gestion de version et la collaboration sur des projets"
+        ],
+        lien: "https://github.com/LabatiJulien/P4-Ohmyfood.git"
+    };
+
+    const settings = {
+        dots: true,
+        infinite: false, 
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
     return (
         <div id="projets" className="projets">
             <h2>Projets</h2>
-            <div className="projet">
-                <h3>Nom du Projet 1</h3>
-                <p>Description : [Description du Projet]</p>
-                <p>Compétences développées : [Compétences]</p>
-                <a href="[Lien vers le code ou le site]" target="_blank" rel="noopener noreferrer">Voir le Projet</a>
-            </div>
-            <div className="projet">
-                <h3>Nom du Projet 2</h3>
-                <p>Description : [Description du Projet]</p>
-                <p>Compétences développées : [Compétences]</p>
-                <a href="[Lien vers le code ou le site]" target="_blank" rel="noopener noreferrer">Voir le Projet</a>
-            </div>
+            <Slider {...settings}>
+                <div className="projet">
+                    <img src={ohMyFoodImage} alt={projet.nom} />
+                    <h3>{projet.nom}</h3>
+                    <p>Description : {projet.description}</p>
+                    <p>Compétences développées : {projet.competences.join(', ')}</p>
+                    <a href={projet.lien} target="_blank" rel="noopener noreferrer">Voir le Projet</a>
+                </div>
+            </Slider>
         </div>
     );
 }
