f857b149d948731eda79d90421643a65a630bdbe
LabatiJulien <julien.labati@hotmail.fr>
2024-06-02 11:13:54 +0200

    Configuration modale projet

--- portfolio/src/components/projets/Projets.js
+++ portfolio/src/components/projets/Projets.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -17,6 +17,17 @@ const Projets = () => {
         lien: "https://github.com/LabatiJulien/P4-Ohmyfood.git"
     };
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const imageRef = useRef(null);
+
+    const handleMouseEnter = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsModalOpen(false);
+    };
+
     const settings = {
         dots: true,
         infinite: false, 
@@ -30,11 +41,30 @@ const Projets = () => {
             <h2>Projets</h2>
             <Slider {...settings}>
                 <div className="projet">
-                    <img src={ohMyFoodImage} alt={projet.nom} />
-                    <h3>{projet.nom}</h3>
-                    <p>Description : {projet.description}</p>
-                    <p>Compétences développées : {projet.competences.join(', ')}</p>
-                    <a href={projet.lien} target="_blank" rel="noopener noreferrer">Voir le Projet</a>
+                    <div 
+                        className="image-container"
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <img ref={imageRef} src={ohMyFoodImage} alt={projet.nom} />
+                        {isModalOpen && (
+                            <div className="modal" style={{ width: imageRef.current.width, height: imageRef.current.height }}>
+                                <h3 style={{ textAlign: 'center' }}>{projet.nom}</h3>
+                                <div className="modal-content">
+                                    <h4>Description :</h4>
+                                    <p>{projet.description}</p>
+                                    <h4>Compétences développées :</h4>
+                                    <ul>
+                                        {projet.competences.map((competence, index) => (
+                                            <li key={index}>{competence}</li>
+                                        ))}
+                                    </ul>
+                                    <h4>Lien :</h4>
+                                    <a href={projet.lien} target="_blank" rel="noopener noreferrer">{projet.lien}</a>
+                                </div>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </Slider>
         </div>
