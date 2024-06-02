bed5af5805079f23b56a2c408bc9ad3f2057406a
LabatiJulien <julien.labati@hotmail.fr>
2024-06-02 17:29:00 +0200

    Boucle du carrousel

--- portfolio/src/components/projets/Projets.js
+++ portfolio/src/components/projets/Projets.js
@@ -51,7 +51,7 @@ const Projets = () => {
 
     const settings = {
         dots: true,
-        infinite: false,
+        infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1
